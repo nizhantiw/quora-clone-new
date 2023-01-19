3aa59cdd9a4d7bf31ff89f458bcb0eddb9fa389f
nizhantiw <nizhantiw@gmail.com>
2023-01-19 22:08:07 +0530

    Update userSlice.js

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -60,7 +60,7 @@ export const { logout, login } = userSlice.actions;
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectUser = state => state.user;
+export const selectUser = state => state.user.user;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
